6149a34ff5f6523f129c9a39c80e9dfa8c80f261
Adetoye daniel kehinde <150145750+DanXtech@users.noreply.github.com>
2024-11-11 11:35:21 +0100

    Update Question.js

--- models/Question.js
+++ models/Question.js
@@ -1,25 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const QuestionSchema = new mongoose.Schema({
-//   chapterId: {
-//     type: String,
-//     enum: ["FA", "FDP", "AE"],
-//     required: true,
-//   },
-//   questionText: { type: String, required: true },
-//   answers: [{ type: String, required: true }],
-//   correctAnswer: { type: Number, required: true },
-//   difficulty: {
-//     type: String,
-//     enum: ["Easy", "Medium", "Hard"],
-//     required: true,
-//   },
-//   hint: { type: String },
-//   chapter: { type: Number, required: true },
-//   page: { type: Number },
-// });
-
-// module.exports = mongoose.model("Question", QuestionSchema);
 
 const mongoose = require("mongoose");
 
@@ -30,8 +8,11 @@ const QuestionSchema = new mongoose.Schema({
     required: true,
   },
   questionText: { type: String, required: true },
-  answers: [{ type: String, required: true }], // Array of strings
+  similarText: {type: String, required: true},
+  answers: [{ type: String, required: true }],
+  similarAnswer: [{ type: String, required: true }],
   correctAnswer: { type: Number, required: true },
+similarCorrect: { type: Number, required: true },
   difficulty: {
     type: String,
     enum: ["Easy", "Medium", "Hard"],
